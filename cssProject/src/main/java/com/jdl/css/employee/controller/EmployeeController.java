1b919d0ccc10dca1b72dd5ad972c8f87dca53d4e
xosle <xosle@DESKTOP-BJEMBN6>
2018-08-22 13:30:34 +0900

    0

--- cssProject/src/main/java/com/jdl/css/employee/controller/EmployeeController.java
+++ cssProject/src/main/java/com/jdl/css/employee/controller/EmployeeController.java
@@ -63,7 +63,7 @@ public class EmployeeController {
 	public ModelAndView login(EmployeeVo employee,HttpSession session, ModelAndView mv){
 		EmployeeVo user = eService.selectEmployeeById(employee.geteId());
 		List<NoteVo> indexNote = nService.selectIndexNote(user.geteKey());
-		
+		System.out.println("user : " + user);
 		//근속년수에 따른 총 휴가 값 가지고오기
 		VacationVo giveVacation = vService.selectTotalVacation(user);
 		//휴가 사용일 가져오기
